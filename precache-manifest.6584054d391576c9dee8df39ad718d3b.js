bba6f7f0bb79251ef0d02e78adc1e7e3c17bfe63
Kajol Tale <kajoltale21@gmail.com>
2020-10-03 20:28:05 +0530

    New URL

---
+++ precache-manifest.6584054d391576c9dee8df39ad718d3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dad77edbda3457a356a10148785c28b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52f443a0ffe6d82a7228",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ac4da0d11c327bb2973",
+    "url": "/static/js/2.6fa69524.chunk.js"
+  },
+  {
+    "revision": "726a29adef7ba1bcdaff8dba444e7b90",
+    "url": "/static/js/2.6fa69524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f443a0ffe6d82a7228",
+    "url": "/static/js/main.f2c528b6.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
